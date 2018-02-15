eb98101c5f756d02cbb9be0fd3defa8f823c015c
TariqAHassan <tariqaahassan@gmail.com>
2018-02-15 15:48:40 -0800

    Move aic and bic into criterion.R

---
+++ R/criterion.R
@@ -0,0 +1,40 @@
+# BIC
+
+#' @export
+criterion <- function(choice){
+    # Choice
+}
+
+
+#' @export
+aic <- function(model){
+    # Compute the Akaike Information Criterion (AIC)
+    #
+    # Args:
+    #   model (R model object): ...
+    #
+    # Returns:
+    #   numeric: ...
+    #
+    # References:
+    #   * https://en.wikipedia.org/wiki/Akaike_information_criterion
+    #
+    return(NULL)
+}
+
+
+#' @export
+bic <- function(model){
+    # Compute the Bayesian Information Criterion (AIC)
+    #
+    # Args:
+    #   model (R model object): ...
+    #
+    # Returns:
+    #   numeric: ...
+    #
+    # References:
+    #   * https://en.wikipedia.org/wiki/Bayesian_information_criterion
+    #
+    return(NULL)
+}
