4c5e9dac367376542ef4c50e0b00b3cf2034b610
Jill Cates <cates.jill@gmail.com>
2018-02-19 09:56:29 -0500

    Add dummy tests for each R function

--- tests/testthat/test-aic.R
+++ tests/testthat/test-aic.R
@@ -1,4 +1,4 @@
-context("test-name.R")
+context("aic.R")
 
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
---
+++ tests/testthat/test-forward.R
@@ -0,0 +1,5 @@
+context("forward.R")
+
+test_that("multiplication works", {
+    expect_equal(2 * 2, 4)
+})
