3468d08abc8511329077128ca9c8b3062da43b67
Jill Cates <cates.jill@gmail.com>
2018-02-20 14:18:59 -0500

    Add more unit tests to aic, bic, forward, backward functions

--- tests/testthat/test-aic.R
+++ tests/testthat/test-aic.R
@@ -1,6 +1,11 @@
 context("aic.R")
 
-test_that("output is float", {
-    output <- aic()
+test_that("output is a float", {
+    output <- aic(X_train, y_train)
     expect_is(output, "numeric")
+})
+
+test_that("error message occurs when input is not correct format", {
+    expect_error(aic("string"), "wrong format for input")
+    expect_error(aic(), "need to pass in X_train and y_train as arguments")
 })
--- tests/testthat/test-bic.R
+++ tests/testthat/test-bic.R
@@ -1,7 +1,14 @@
 context("bic.R")
 
-
-test_that("output is float", {
-    output <- bic()
+test_that("output is a float", {
+    output <- bic(X_train, y_train)
     expect_is(output, "numeric")
 })
+
+test_that("error message occurs when input is not correct format", {
+    expect_error(bic("string"), "wrong format for input")
+    expect_error(bic(), "need to pass in X_train and y_train as arguments")
+})
+
+
+
