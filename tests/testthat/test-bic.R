a8b913cd84ebb9901c8e015279d9606a045b7b6d
ap26793 <zi.avi7@gmail.com>
2018-02-26 11:18:34 -0800

    Removed lambda testcases for bic

--- tests/testthat/test-bic.R
+++ tests/testthat/test-bic.R
@@ -1,21 +1,17 @@
 context("bic.R")
 
 test_that("output is a float", {
-    output <- bic(model, 1)
+    output <- bic(model)
     expect_is(output, "numeric")
 })
 
 test_that("error message occurs when input is not correct format", {
-    expect_error(bic(2,1), "Wrong format for model input")
-    expect_error(bic(model,"lambda"), "Wrong format for lambda input")
-    expect_error(bic(), "Need to pass in model and lambda as arguments")
+    expect_error(bic(2), "Wrong format for model input")
+    expect_error(bic(), "Need to pass in model as arguments")
 })
 
 test_that("bic is correct", {
-    expect_equal(bic(model_1, 1), value_1)
-    expect_equal(bic(model_2, 1), value_2)
+    expect_equal(bic(model_1), value_1)
+    expect_equal(bic(model_2), value_2)
 })
 
-test_that("lambda value is an optional argument", {
-    expect_equal(bic(model), value)
-})
