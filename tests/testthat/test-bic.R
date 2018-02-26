9f61a1deaada99ff57895fedf051dcec0bfa1b31
ap26793 <zi.avi7@gmail.com>
2018-02-26 10:35:34 -0800

    Added test case for lambda value is an optional argument

--- tests/testthat/test-bic.R
+++ tests/testthat/test-bic.R
@@ -6,9 +6,9 @@ test_that("output is a float", {
 })
 
 test_that("error message occurs when input is not correct format", {
-    expect_error(bic(2,1), "wrong format for model input")
-    expect_error(bic(model,"lambda"), "wrong format for lambda input")
-    expect_error(bic(), "need to pass in model and lambda as arguments")
+    expect_error(bic(2,1), "Wrong format for model input")
+    expect_error(bic(model,"lambda"), "Wrong format for lambda input")
+    expect_error(bic(), "Need to pass in model and lambda as arguments")
 })
 
 test_that("bic is correct", {
@@ -16,3 +16,6 @@ test_that("bic is correct", {
     expect_equal(bic(model_2, 1), value_2)
 })
 
+test_that("lambda value is an optional argument", {
+    expect_equal(bic(model), value)
+})
