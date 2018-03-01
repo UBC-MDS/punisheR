47c8638f5ce4a4b4da99295794fa5fc78c38d996
ap26793 <zi.avi7@gmail.com>
2018-03-01 14:38:18 -0800

    Removed lambda input for test functions

--- tests/testthat/test-aic.R
+++ tests/testthat/test-aic.R
@@ -1,16 +1,16 @@
 context("aic.R")
 
 test_that("output is a float", {
-    output <- aic(model, 1)
+    output <- aic(model)
     expect_is(output, "numeric")
 })
 
 test_that("error message occurs when input is not correct format", {
-    expect_error(aic(2,1), "Wrong format for model input")
+    expect_error(aic(2), "Wrong format for model input")
     expect_error(aic(), "Need to pass in model and lambda as arguments")
 })
 
 test_that("aic is correct", {
-    expect_equal(aic(model_1, 1), value_1)
-    expect_equal(aic(model_2, 1), value_2)
+    expect_equal(aic(model_1), value_1)
+    expect_equal(aic(model_2), value_2)
 })
