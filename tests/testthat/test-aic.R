44c22a7ddbdf307a9b099658c8aaf077d3f8dffb
ap26793 <zi.avi7@gmail.com>
2018-03-01 14:40:27 -0800

    Updated test descriptions

--- tests/testthat/test-aic.R
+++ tests/testthat/test-aic.R
@@ -1,16 +1,16 @@
 context("aic.R")
 
-test_that("output is a float", {
+test_that("check if output is a float", {
     output <- aic(model)
     expect_is(output, "numeric")
 })
 
-test_that("error message occurs when input is not correct format", {
+test_that("error message is thrown when input is not in correct format", {
     expect_error(aic(2), "Wrong format for model input")
     expect_error(aic(), "Need to pass in model and lambda as arguments")
 })
 
-test_that("aic is correct", {
+test_that("check if aic value returned is correct", {
     expect_equal(aic(model_1), value_1)
     expect_equal(aic(model_2), value_2)
 })
