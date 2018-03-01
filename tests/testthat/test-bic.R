f9d4fa904b23bcc0a5a03d6c896e9e3657ea8682
ap26793 <zi.avi7@gmail.com>
2018-03-01 15:27:02 -0800

    Updated value test case

--- tests/testthat/test-bic.R
+++ tests/testthat/test-bic.R
@@ -10,8 +10,15 @@ test_that("error message occurs when input is not correct format", {
     expect_error(bic(), "Need to pass in model as an argument")
 })
 
-test_that("bic is correct", {
-    expect_equal(bic(model_1), value_1)
-    expect_equal(bic(model_2), value_2)
+test_that("check if the bic value returned is correct", {
+
+    model_1 <- lm(formula = Petal.Length~Sepal.Length, data = iris)
+    expected_value1 <- BIC(object = model_1)
+    expect_equal(bic(model_1), expected_value1)
+
+    model_2 <- lm(formula = dist~speed, data = cars)
+    expected_value2 <- BIC(object = model_2)
+    expect_equal(bic(model_2), expected_value2)
+
 })
 
