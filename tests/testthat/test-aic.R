3021f2be0b83572b7adb6b17afd890a94ab33c72
ap26793 <zi.avi7@gmail.com>
2018-03-01 14:36:18 -0800

    Removed lambda test-cases

--- tests/testthat/test-aic.R
+++ tests/testthat/test-aic.R
@@ -7,15 +7,10 @@ test_that("output is a float", {
 
 test_that("error message occurs when input is not correct format", {
     expect_error(aic(2,1), "Wrong format for model input")
-    expect_error(aic(model,"lambda"), "Wrong format for lambda input")
     expect_error(aic(), "Need to pass in model and lambda as arguments")
 })
 
 test_that("aic is correct", {
     expect_equal(aic(model_1, 1), value_1)
     expect_equal(aic(model_2, 1), value_2)
-})
-
-test_that("lambda value is an optional argument", {
-    expect_equal(aic(model), value)
 })
