d26e3015fcaac7eaac2f7caebbd93139459badc3
Jill Cates <cates.jill@gmail.com>
2018-03-04 09:05:15 -0800

    Another fix to forward tests...added max_featrues val

--- tests/testthat/test-forward.R
+++ tests/testthat/test-forward.R
@@ -13,28 +13,28 @@ test_that("output is a vector", {
 
 test_that("error message occurs when input is not correct format", {
     expect_error(forward(1234, X_train, y_train, X_val, y_val,
-                    min_change=0.5, max_features, criterion='aic',
+                    min_change=0.5, max_features=2, criterion='aic',
                     verbose=TRUE), "`model` not a Base-R Model.")
     expect_error(forward(model, 1234, y_train, X_val, y_val,
-                    min_change=0.5, max_features, criterion='aic',
+                    min_change=0.5, max_features=2, criterion='aic',
                     verbose=TRUE), "`X_train` matrix is not a 2D matrix.")
     expect_error(forward(model, X_train, 1234, X_val, y_val,
-                    min_change=0.5, max_features, criterion='aic',
+                    min_change=0.5, max_features=2, criterion='aic',
                     verbose=TRUE), "`y_train` is not a 1D vector.")
     expect_error(forward(model, X_train, y_train, 1234, y_val,
-                    min_change=0.5, max_features, criterion='aic',
+                    min_change=0.5, max_features=2, criterion='aic',
                     verbose=TRUE), "`X_val` is not a 2D matrix.")
     expect_error(forward(model, X_train, y_train, X_val, 1234,
-                    min_change=0.5, max_features, criterion='aic',
+                    min_change=0.5, max_features=2, criterion='aic',
                     verbose=TRUE), "`y_val` is not a 1D vector.")
     expect_error(forward(model, X_train, y_train, X_val, y_val,
                     min_change=0.5, "abc", criterion='aic',
                     verbose=TRUE), "`max_features` is not of type `int`")
     expect_error(forward(model, X_train, y_train, X_val, y_val,
-                    min_change=0.5, -2, criterion='aic',
+                    min_change=0.5, max_features=-2, criterion='aic',
                     verbose=TRUE), "`max_features` should be a positive `int`")
     expect_error(forward(model, X_train, y_train, X_val, y_val,
-                    min_change=0.5, max_features, "abc",
+                    min_change=0.5, max_features=2, "abc",
                     verbose=TRUE), "unexpected `criterion`")
     expect_error(forward(), "No defaults specified for model, `X_train`, `y_train`, `X_val`, `y_val`, `max_features`")
 })
