b3fa6963b8ea0d5aa39032213a4f404ce4dbc1b3
ap26793 <zi.avi7@gmail.com>
2018-03-08 14:42:41 -0800

    Incoporated all the changed due to vignette

--- R/criterion.R
+++ R/criterion.R
@@ -4,8 +4,24 @@
 
 
 #' @export
+aic <- function(model){
+    # Compute the Akaike Information Criterion (AIC)
+    #
+    # Args:
+    #   model (R model object): ...
+    #
+    # Returns:
+    #   numeric: ...
+    #
+    # References:
+    #   * https://en.wikipedia.org/wiki/Akaike_information_criterion
+    #
+    return(NULL)
+}
+
+
 bic <- function(model){
-    # Compute the Bayesian Information Criterion (AIC)
+    # Compute the Bayesian Information Criterion (BIC)
     #
     # Args:
     #   model (R model object): ...
