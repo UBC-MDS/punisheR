0b22572b39adeec709301b21878e11de45e1793f
Jill Cates <cates.jill@gmail.com>
2018-03-09 21:31:28 -0800

    Add placehold roxygen2 formatted docs for the rest of the functions

--- R/criterion.R
+++ R/criterion.R
@@ -1,9 +1,23 @@
-# Criterion
-
-
-
-
+#' Bayesian Information Criterion
+#' 
+#' @description
+#' This is an implementation of the backward selection algorithm
+#' that can be used to select best features in model. 
+#' 
+#' @param  model Base R model  
+#'
+#' @param X_train Training data
+#' 
+#'  A 2D matrix of (observations, features)
+#'  
+#' @param y_train Target class for training data
+#' 
+#' @return bic value (float)
+#' 
 #' @export
+
+
+
 bic <- function(model){
     # Compute the Bayesian Information Criterion (AIC)
     #
