436461105bb2273171b4c43816d6f9189547fee5
TariqAHassan <tariqaahassan@gmail.com>
2018-03-09 20:35:57 -0800

    Remove reference to `model`, add ToDos

--- tests/testthat/test-forward.R
+++ tests/testthat/test-forward.R
@@ -1,5 +1,6 @@
 context("forward.R")
 
+# ToDo: use the data from `data_for_tests.R`
 X_train <- data.frame(x1=rnorm(10), x2=rep(0,10), x3=rnorm(10), x4=rep(0, 10))
 y_train <- rnorm(10)
 X_val <- data.frame(x1=rnorm(10), x2=rep(0,10), x3=rnorm(10), x4=rep(0, 10))
@@ -12,35 +13,32 @@ test_that("output is a vector", {
 })
 
 test_that("error message occurs when input is not correct format", {
-    expect_error(forward(1234, X_train, y_train, X_val, y_val,
-                    min_change=0.5, max_features=2, criterion='aic',
-                    verbose=TRUE), "`model` not a Base-R Model.")
-    expect_error(forward(model, 1234, y_train, X_val, y_val,
+    expect_error(forward(1234, y_train, X_val, y_val,
                     min_change=0.5, max_features=2, criterion='aic',
                     verbose=TRUE), "`X_train` matrix is not a 2D matrix.")
-    expect_error(forward(model, X_train, 1234, X_val, y_val,
+    expect_error(forward(X_train, 1234, X_val, y_val,
                     min_change=0.5, max_features=2, criterion='aic',
                     verbose=TRUE), "`y_train` is not a 1D vector.")
-    expect_error(forward(model, X_train, y_train, 1234, y_val,
+    expect_error(forward(X_train, y_train, 1234, y_val,
                     min_change=0.5, max_features=2, criterion='aic',
                     verbose=TRUE), "`X_val` is not a 2D matrix.")
-    expect_error(forward(model, X_train, y_train, X_val, 1234,
+    expect_error(forward(X_train, y_train, X_val, 1234,
                     min_change=0.5, max_features=2, criterion='aic',
                     verbose=TRUE), "`y_val` is not a 1D vector.")
-    expect_error(forward(model, X_train, y_train, X_val, y_val,
+    expect_error(forward(X_train, y_train, X_val, y_val,
                     min_change=0.5, "abc", criterion='aic',
                     verbose=TRUE), "`max_features` is not of type `int`")
-    expect_error(forward(model, X_train, y_train, X_val, y_val,
+    expect_error(forward(X_train, y_train, X_val, y_val,
                     min_change=0.5, max_features=-2, criterion='aic',
                     verbose=TRUE), "`max_features` should be a positive `int`")
-    expect_error(forward(model, X_train, y_train, X_val, y_val,
+    expect_error(forward(X_train, y_train, X_val, y_val,
                     min_change=0.5, max_features=2, "abc",
                     verbose=TRUE), "unexpected `criterion`")
     expect_error(forward(), "No defaults specified for model, `X_train`, `y_train`, `X_val`, `y_val`, `max_features`")
 })
 
 test_that("forward() selects the best features", {
-    output <- forward(model, X_train, y_train, X_val, y_val,
+    output <- forward(X_train, y_train, X_val, y_val,
                      n_features=2, min_change=0.5, criterion='aic',
                      verbose=TRUE)
     expect_output(output, list(1,4))
