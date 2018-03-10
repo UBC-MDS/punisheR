5bb37172a2c1664b5b07ade699164868b4429eeb
Jill Cates <cates.jill@gmail.com>
2018-03-09 21:21:01 -0800

    Update documentation of forward selection

--- R/forward.R
+++ R/forward.R
@@ -1,37 +1,48 @@
-# Forward Selection
-
+#' Forward Selection Algorithm.
+#' 
+#' @description
+#' This is an implementation of the forward selection algorithm
+#' that can be used to select best features in model. 
+#' 
+#' @param  model Base R model  
+#'
+#' @param X_train Training data
+#' 
+#'  A 2D matrix of (observations, features)
+#'  
+#' @param y_train Target class for training data
+#' 
+#'  A 1D array of target classes for X_train 
+#'  
+#' @param X_val Validation data
+#'
+#'  A 2D matrix of (observations, features)
+#'  
+#' @param y_val Target class for validation data
+#' 
+#'  A 1D array of target classes for X_val
+#' 
+#' @param criterion Model selection criterion
+#'  A criterion to measure relative model quality. 
+#'  'aic': use Akaike Information Criterion
+#'  'bic': use Bayesian Information Criterion
+#' @param min_change Smallest change in criterion score to be considered significant.
+#'   
+#' @param max_features Maximum number of features to allow.
+#' 
+#' @param verbose 
+#'  if True, print additional information as selection occurs  
+#' @return A vector
 #' @export
+#' @examples 
+#' model = lm(Sepal.Length~Sepal.Width+Petal.Length+Petal.Width, data=iris)
+#' forward(model, X_train, y_train, min_change=0.3, criterion='aic')
+#' 
+#' @export
+ 
+
 forward <- function(model, X_train, y_train, X_val, y_val,
                     min_change=0.5, max_features, criterion,
                     verbose=TRUE){
-    # Forward Selection Algorithm.
-    #
-    # Args:
-    #     model : model
-    #         Base R Model.
-    #     X_train matrix
-    #         a 2D matrix of (observations, features).
-    #     y_train : matrix
-    #         a 1D array of target classes for X_train.
-    #     X_val : matrix
-    #         a 2D matrix of (observations, features).
-    #     y_val : matrix
-    #         a 1D array of target classes for X_validate.
-    #     criterion : char
-    #         model selection criterion.
-    #         * 'aic': use Akaike Information Criterion.
-    #         * 'bic': use Bayesian Information Criterion.
-    #     min_change : int or float, optional
-    #         The smallest change to be considered significant.
-    #     max_features : int
-    #         the max. number of features to allow.
-    #     verbose : bool
-    #         if True, print additional information as selection occurs.
-    #         Defaults to True.
-    #
-    # Returns:
-    #     S : list
-    #       The column indices of `X_train` (and `X_val`)
-    #       that denote the chosen features.
     return(NULL)
 }
