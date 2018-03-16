0b42a0704a4c991b972da5e0535589ead3df45fb
TariqAHassan <tariqaahassan@gmail.com>
2018-03-15 21:20:23 -0700

    Add error handling

--- R/criterion.R
+++ R/criterion.R
@@ -9,6 +9,9 @@
 #'
 #' @keywords internal
 .rss_calc <- function(model, X, y){
+    if (is.null(X) | is.null(y)) {
+        stop("Neither `X` nor `y` can be NULL.")
+    }
     df <- as.data.frame(X)
     y_pred <- suppressWarnings(predict(model, df))
     rss <- sum((y - y_pred)^2)
@@ -52,7 +55,13 @@ r_squared <- function(fit_model, X, y){
 
     n <- length(model$residuals)
     k <- model$rank + 1
-    rss <- .rss_calc(model, X = X, y = y)
+    if (is.null(X) & is.null(y)){
+        rss <- sum(model$residuals ^ 2)
+    } else if (is.null(X) | is.null(y)) {
+        stop("if `X` or `y` is NULL, they both must be.")
+    } else {
+        rss <- .rss_calc(model, X = X, y = y)
+    }
     llf <- -1 * (n / 2) * log(2 * pi) - (n / 2) * log(rss / n) - n / 2
     return(c(n, k, llf))
 }
@@ -68,15 +77,16 @@ r_squared <- function(fit_model, X, y){
 #'
 #' @param model A base R model object (e.g., \code{lm()})
 #'
-#' @param X Feature data
+#' @param X Feature data. If NULL, extract `X` from `model`.
 #'
 #' @param y True labels of the response variable
+#'          If NULL, extract `y` from `model`.
 #'
 #' @return  AIC value gets returned as a float.
 #'
 #' @references https://en.wikipedia.org/wiki/Akaike_information_criterion
 #' @export
-aic <- function(model, X, y){
+aic <- function(model, X=NULL, y=NULL){
     # Calculate AIC
     coeff <- .get_coeffs(model, X = X, y = y)
     k <- coeff[2]
@@ -97,15 +107,16 @@ aic <- function(model, X, y){
 #'
 #' @param  model A base R model object (e.g., \code{lm()})
 #'
-#' @param X Feature data
+#' @param X Feature data. If NULL, extract `X` from `model`.
 #'
-#' @param y True labels of the response variable
+#' @param y True labels of the response variable.
+#'          If NULL, extract `y` from `model`.
 #'
 #' @return BIC value gets returned as a flaot.
 #'
 #' @references https://en.wikipedia.org/wiki/Bayesian_information_criterion
 #' @export
-bic <- function(model, X, y){
+bic <- function(model, X=NULL, y=NULL){
     # Calcualte BIC
     coeff <- .get_coeffs(model, X = X, y = y)
     n <- coeff[1]
