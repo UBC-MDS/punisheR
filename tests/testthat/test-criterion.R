b15bd14ecdf18090da2488c41a1bb92e4b951fb9
TariqAHassan <tariqaahassan@gmail.com>
2018-03-16 02:19:53 -0700

    Add manual tests

--- tests/testthat/test-criterion.R
+++ tests/testthat/test-criterion.R
@@ -1,4 +1,4 @@
-#context("criterion.R")
+context("criterion.R")
 
 # -----------------------------------------------------------------------------
 # Setup
@@ -43,4 +43,57 @@ test_that("test_metric_output_value", {
     # our functions match that computed by Base R.
     expect_equal(aic(model), AIC(model))
     expect_equal(bic(model), BIC(model))
+})
+
+# -----------------------------------------------------------------------------
+# Output Value
+# -----------------------------------------------------------------------------
+
+# Note: we have replicated code here, which is not D.R.Y.
+# However, we have done so to show, in detail, the manual
+# calculations of AIC and BIC occuring explictly.
+
+test_that("test aic() against manual calculation.", {
+    # Test *very* simple case of `y = x`:
+    N <- 100
+    values <- 1:N
+    data <- data.frame(x=values, y=values)
+    rank <- 2
+    model <- lm(formula = y ~ x, data = data)
+
+    # Manual Calculation -------------------------------
+    n <- N
+    k <- rank + 1
+    # Compute RSS ---
+    extract_residuals <- residuals(model)
+    rss <- sum(extract_residuals ^ 2)
+    llf <- -1 * (n / 2) * log(2 * pi) - (n / 2) * log(rss / n) - n / 2
+    manually_calculated_aic <- -2 * llf + 2 * k
+
+    # Ours ---------------------------------------------
+    function_aic_value <- aic(model)
+    expect_equal(function_aic_value, manually_calculated_aic)
+})
+
+
+test_that("test bic() against manual calculation.", {
+    # Test *very* simple case of `y = x`:
+    N <- 100
+    values <- 1:N
+    data <- data.frame(x=values, y=values)
+    rank <- 2
+    model <- lm(formula = y ~ x, data = data)
+
+    # Manual Calculation -------------------------------
+    n <- N
+    k <- rank + 1
+    # Compute RSS ---
+    extract_residuals <- residuals(model)
+    rss <- sum(extract_residuals ^ 2)
+    llf <- -1 * (n / 2) * log(2 * pi) - (n / 2) * log(rss / n) - n / 2
+    manually_calculated_bic <- -2 * llf + log(n) * k
+
+    # Ours ---------------------------------------------
+    function_bic_value <- bic(model)
+    expect_equal(function_bic_value, manually_calculated_bic)
 })
